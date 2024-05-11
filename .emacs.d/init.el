0e6347c8a3bcfcf5558103b468fb3e092e2f1ef4
daniloinacio <daniloinaciossilva@gmail.com>
2024-05-11 06:14:26 -0300

    Update Emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,4 +12,53 @@
 ;; Display line numbers in every buffer
 (global-display-line-numbers-mode 1)
 
-(load-theme 'deeper-blue t)
+(put 'upcase-region 'disabled nil)
+
+
+;;; Startup
+;;; PACKAGE LIST
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+	("org" . "https://orgmode.org/elpa/")
+	("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; BOOTSTRAP USE-PACKAGE
+(package-initialize)
+(setq use-package-always-ensure t)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile (require 'use-package))
+
+;;; UNDO
+;; Vim style undo not needed for emacs 28
+(use-package undo-fu)
+
+;;; Vim Bindings
+(use-package evil
+  :demand t
+  :bind (("<escape>" . keyboard-escape-quit))
+  :init
+  ;; allows for using cgn
+  ;; (setq evil-search-module 'evil-search)
+  (setq evil-want-keybinding nil)
+  ;; no vim insert bindings
+  (setq evil-undo-system 'undo-fu)
+  :config
+  (evil-mode 1))
+
+;; Vim Bindings Everywhere else
+(use-package evil-collection
+  :after evil
+  :config
+  (setq evil-want-integration t)
+  (evil-collection-init))
+
+
+(use-package gruber-darker-theme
+  :init
+  (load-theme 'gruber-darker t))
+
+(use-package haskell-mode) 
+(use-package markdown-mode)
+(use-package rust-mode)
